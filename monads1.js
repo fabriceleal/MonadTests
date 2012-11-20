1d70147de04ee00513faeb8fd1f4c6394adeb0e1
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2012-11-20 22:21:58 +0000

    continuation monad seems better ...

--- monads1.js
+++ monads1.js
@@ -1,144 +1,167 @@
 /*
-	http://blogs.msdn.com/b/wesdyer/archive/2008/01/11/the-marvels-of-monads.aspx
-	
-	Translated from C#/Linq to Javascript
+    http://blogs.msdn.com/b/wesdyer/archive/2008/01/11/the-marvels-of-monads.aspx
+    
+    Translated from C#/Linq to Javascript
 */
 
 // The identity monad simply wraps a value.
 var IdentityMonad = function(scalarValue){
-	this.value = function(){
-		return scalarValue;
-	};
-	Object.freeze(this);
+    this.value = function(){
+        return scalarValue;
+    };
+    Object.freeze(this);
 };
 
 // The maybe monad might hold a value, or not
 var MaybeMonad = function() {
-	var _args = Array.prototype.slice.call(arguments);
-	if(this.hasValue = (_args.length > 0)){
-		this.value = function(){
-			return _args[0];
-		};
-	} else {
-		//console.log('nothing!');
-		
-		if(! MaybeMonad.Nothing) {
-			MaybeMonad.Nothing = this;
-		} else {
-			return MaybeMonad.Nothing;
-		}
-		console.log('value()');
-		// Only hit if initing
-		this.value = function(){
-			// return undefined;
-		};
-	}	
-	Object.freeze(this);
+    var _args = Array.prototype.slice.call(arguments);
+    if(this.hasValue = (_args.length > 0)){
+        this.value = function(){
+            return _args[0];
+        };
+    } else {
+        //console.log('nothing!');
+        
+        if(! MaybeMonad.Nothing) {
+            MaybeMonad.Nothing = this;
+        } else {
+            return MaybeMonad.Nothing;
+        }
+        console.log('value()');
+        // Only hit if initing
+        this.value = function(){
+            // return undefined;
+        };
+    }	
+    Object.freeze(this);
 }
 
 MaybeMonad.Nothing = new MaybeMonad();
 
 // The list monad
 var ListMonad = function(scalarValue){
-	
-	if(scalarValue instanceof Array) {
-		//console.log('is array!');
-		
-		this.value = function(){
-			return scalarValue;
-		};
-	} else {
-		//console.log('is not array!');
-		
-		this.value = function(){
-			return [scalarValue];
-		};
-	}
-	
-	Object.freeze(this);
+    
+    if(scalarValue instanceof Array) {
+        //console.log('is array!');
+        
+        this.value = function(){
+            return scalarValue;
+        };
+    } else {
+        //console.log('is not array!');
+        
+        this.value = function(){
+            return [scalarValue];
+        };
+    }
+    
+    Object.freeze(this);
 };
 
 var ContinuationMonad = function(transformFunction) {
-	this.value = function(){
-		return transformFunction;
-	};
-	Object.freeze(this);
+    this.value = function(){
+        return transformFunction;
+    };
+    Object.freeze(this);
 };
 
 // *** Bind ***
 IdentityMonad.prototype.bind = function(arrowOfTreturnsIdentityOfU, getsTwoValuesEval) {
-	if(getsTwoValuesEval) {
-		return getsTwoValuesEval(this.value(), 
-								arrowOfTreturnsIdentityOfU(this.value()).value()
-								).toIdentity();
-	} else {
-		return arrowOfTreturnsIdentityOfU(this.value());
-	}
+    if(getsTwoValuesEval) {
+        return getsTwoValuesEval(this.value(), 
+                                arrowOfTreturnsIdentityOfU(this.value()).value()
+                                ).toIdentity();
+    } else {
+        return arrowOfTreturnsIdentityOfU(this.value());
+    }
 };
 
 MaybeMonad.prototype.bind = function(arrowOfTreturnsMaybeOfU){
-	if(! this.hasValue){
-		return MaybeMonad.Nothing;
-	}
-	return arrowOfTreturnsMaybeOfU(this.value());
+    if(! this.hasValue){
+        return MaybeMonad.Nothing;
+    }
+    return arrowOfTreturnsMaybeOfU(this.value());
 };
 
 ListMonad.prototype.bind = function(arrowOfTreturnsListOfU){
-	var tmp = [];
-	var thisValue = this.value();
-	
-	for(var x = 0; x < thisValue.length ; ++x) {
-		(function(x){			
-			// x is the element
-			var resValue = arrowOfTreturnsListOfU(x).value();
-			for(var y = 0; y < resValue.length; ++y) {
-					(function(y){
-						tmp.push(y);
-					})(resValue[y]);
-			}			
-		})(thisValue[x]);
-	}
-	return tmp.toList();
+    var tmp = [];
+    var thisValue = this.value();
+    
+    for(var x = 0; x < thisValue.length ; ++x) {
+        (function(x){			
+            // x is the element
+            var resValue = arrowOfTreturnsListOfU(x).value();
+            for(var y = 0; y < resValue.length; ++y) {
+                    (function(y){
+                        tmp.push(y);
+                    })(resValue[y]);
+            }			
+        })(thisValue[x]);
+    }
+    return tmp.toList();
 };
 
-ContinuationMonad.prototype.bind = function(arrowOfTreturnsContinuationOfUAnswer){	
-	return new ContinuationMonad(function(c) {
-		return this.value()(function(x){
-			return arrowOfTreturnsContinuationOfUAnswer(x);
-		})(c);
-	});
+ContinuationMonad.prototype.bind = function(arrowOfTreturnsContinuationOfUAnswer){
+    var k = arrowOfTreturnsContinuationOfUAnswer;
+    var _this = this;
+    var c_tores = function(c) {
+        //console.log('DBG bind: c must be a function');
+        //console.log(c.toString());
+        
+        var _f = _this.value();
+        //console.log('DBG bind: _f must be a function');
+        //console.log(_f.toString());
+        
+        return _f(function(x){
+            //console.log('DBG bind: x can be anything');
+            //console.log(x);
+            
+            //console.log('DBG bind: k must be a function that returns a continuationmonad');
+            
+            return k(x).value();			
+        })(c);
+    };
+    return new ContinuationMonad(c_tores);
 };
 
 // *** Unit ***
 
 Object.prototype.toMaybe = function(){
-	return new MaybeMonad(this);
+    return new MaybeMonad(this);
 };
 
 Object.prototype.toIdentity = function(){
-	return new IdentityMonad(this);
+    return new IdentityMonad(this);
 };
 
 Object.prototype.toList = function(){
-	return new ListMonad(this);
+    return new ListMonad(this);
 };
 
 Object.prototype.toContinuation = function(){
-	var _this = this;
-	return function(c){
-		return c(_this);
-	};
+    var _this = this;
+    // Returns a continuation monad, that wraps a call 
+    // to a function that is called with this value
+    return new ContinuationMonad(function(c){
+        //console.log('c should be a normal function');
+        //console.log('c = ', c);
+        
+        if(c instanceof Function){        
+            return c(_this);
+        }
+        throw new Error('c is not a function!!!');
+    });
 };
 
 // **** Tests ****
 var r;
 
 r = (5).toIdentity().bind(
-	function(){
-		return (6).toIdentity();
-	}, function(x,y){
-		return x+y
-	});
+    function(){
+        return (6).toIdentity();
+    }, function(x,y){
+        return x+y
+    });
 
 // This needs to be like this because r.value() 
 // will return an instance to a Number
@@ -146,36 +169,59 @@ r = (5).toIdentity().bind(
 console.log('id test =', JSON.stringify(r.value()));
 
 r = (5).toMaybe().bind(
-	function(x){ 
-		var mm; 
-		mm = (7).toMaybe();
-		//mm = MaybeMonad.Nothing;
-		return mm.bind(function(y){
-			return (x+y).toMaybe();
-		});
-	});
+    function(x){ 
+        var mm; 
+        mm = (7).toMaybe();
+        //mm = MaybeMonad.Nothing;
+        return mm.bind(function(y){
+            return (x+y).toMaybe();
+        });
+    });
 
 console.log('maybe test =', JSON.stringify(r.value()));
 
 r = [0,1,2].toList().bind(
-		function(x){
-			return [0,1,2].toList().bind(
-				function(y) {
-					return (x+y).toList(); //(x+y).toList();
-				}
-			);
-		});
+        function(x){
+            return [0,1,2].toList().bind(
+                function(y) {
+                    return (x+y).toList(); //(x+y).toList();
+                }
+            );
+        });
 
 console.log('list test =', JSON.stringify(r.value()));
 
+/*
 r = (7).toContinuation().bind(
-	function(x){
-		return (8).toContinuation().bind(
-			function(y){
-				return (x+y);
-			});
-	});
+    function(x){
+        console.log('x=', JSON.stringify(x));
+        return (5).toContinuation().bind(
+            function(y){
+                console.log('y=', JSON.stringify(y));
+                return (x+y);
+            });
+    });
 
-r(function(val){
-	console.log('continuation test =', JSON.stringify(val));
+console.log('continuation test=', r.bind(function(val){
+    console.log('val =', JSON.stringify(val));
+    return val + 1;
+})));
+*/
+
+// Wrap in continuationMonad
+r = (7).toContinuation();
+// This returns the value wrapped in a continuation, as-is
+v = r.value()(function(v){
+    return v;
 });
+console.log('continuation (7)=', JSON.stringify(v));
+
+r = (7).toContinuation().bind(
+    function(x){
+        return (8).toContinuation();
+    });
+
+v = r.value()(function(v){
+    return v;
+});
+console.log('continuation (8)=', JSON.stringify(v));
